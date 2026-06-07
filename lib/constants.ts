7eca3cf80f86a2d2921cf94eae390529ad18b167
aman <zoho7aman@gmail.com>
2026-06-07 23:20:41 +0530

    contact@
    contact@

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const SITE_NAME = "Redecorporativa";
 export const SITE_URL = "https://www.redecorporativa.in";
-export const SITE_EMAIL = "admin@redecorporativa.in";
+export const SITE_EMAIL = "contact@redecorporativa.in";
 export const SITE_PHONE = "+917999645921";
 export const SITE_PHONE_DISPLAY = "+91 79996 45921";
 export const SITE_ADDRESS = "684 Noida, UP, India";
