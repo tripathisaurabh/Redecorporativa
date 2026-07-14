5e29b91722840603c38a3dec43b8d5022f95f2c7
aman <zoho7aman@gmail.com>
2026-07-14 20:24:50 +0530

    mc clarity
    mc clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,6 +80,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           strategy="afterInteractive"
         />
         <Script id="zf-adv-lead" src="/zf-adv-lead.js" strategy="afterInteractive" />
+        <Script
+          id="ms-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "xmdc8uyvhk");`
+          }}
+        />
       </body>
     </html>
   );
